253689df5a65b9ece40dab78f5f5a4fc34a679d9
Bertrik Sikken <bertrik@sikken.nl>
2021-04-18 00:29:37 +0200

    Implement LD303 protocol.

--- protocol.cpp
+++ protocol.cpp
@@ -1,26 +1,100 @@
-
-
 #include "protocol.h"
 
 
-size_t Protocol::build_tx(uint8_t *buf, uint8_t cmd, uint16_t data)
+Protocol::Protocol(void)
+{
+    _state = STATE_HEADER_55;
+    _len = 0;
+}
+
+// protocol 6
+// 55 5A 02 D3 84  open/close, "fixed query command"
+//
+// BA AB .... set parameter
+// 
+// change query mode: BAAB 00 F6 0007 00 55BB = send cmd F6 with parameter 0007
+// -> reports data continuously?
+
+size_t Protocol::build_query(uint8_t *buf, uint8_t param)
 {
     int idx = 0;
-    buf[idx++] = 0xBA;
+    uint8_t sum = 0;
+
+    buf[idx++] = 0x55;  // header
+    buf[idx++] = 0x5A;
+    buf[idx++] = 0x02;  // length
+    buf[idx++] = param;  // query parameter
+    for (int i = 0; i < idx; i++) {
+        sum += buf[idx];
+    }
+    buf[idx++] = sum;
+
+    return idx;
+}
+
+size_t Protocol::build_command(uint8_t *buf, uint8_t cmd, uint16_t data)
+{
+    int idx = 0;
+    buf[idx++] = 0xBA;  // header
     buf[idx++] = 0xAB;
+    buf[idx++] = 0x00;  // address
     buf[idx++] = cmd;
     buf[idx++] = (data >> 8) & 0xFF;
     buf[idx++] = data & 0xFF;
-    buf[idx++] = 0;
+    buf[idx++] = 0;     // checksum
     buf[idx++] = 0x55;
     buf[idx++] = 0xBB;
 
     return idx;
 }
 
+// message from radar: "uplink data, (sent by module)" 
+// 55       A5      0A      XX      XX      XX      XX      XX      XX      XX      XX      XX      XX
+// header           len     adress  distance        rsvd    state   signal strength micro   closed  check
 
 bool Protocol::process_rx(uint8_t c, uint8_t cmd)
 {
+    switch (_state) {
+    case STATE_HEADER_55:
+        if (c == 0x55) {
+            _state = STATE_HEADER_A5;
+            _sum = c;
+        }
+        break;
+    case STATE_HEADER_A5:
+        _sum += c;
+        if (c == 0xA5) {
+            _state = STATE_LEN;
+        } else {
+            _state = STATE_HEADER_55;
+        }
+        break;
+    case STATE_LEN:
+        _sum += c;
+        if (c < sizeof(_buf)) {
+            _len = c;
+            _idx = 0;
+            _state = STATE_DATA;
+        } else {
+            _state = STATE_HEADER_55;
+        }
+        break;
+    case STATE_DATA:
+        _sum += c;
+        if (_idx < _len) {
+            _buf[_idx++] = c;
+        } else {
+            _state = STATE_CHECK;
+        }
+        break;
+    case STATE_CHECK:
+        _state = STATE_HEADER_55;
+        return (c == _sum);
+
+    default:
+        _state = STATE_HEADER_55;
+        break;
+    }
     return false;
 }
 
--- protocol.h
+++ protocol.h
@@ -2,7 +2,6 @@
 #include <stdbool.h>
 #include <stddef.h>
 
-
 // command definitions
 typedef enum {
     CMD_OPERATING_MODE = 0xB1,      // 0 = sensitive, 1 = stable
@@ -14,23 +13,46 @@ typedef enum {
     CMD_OUTPUT_TARGET = 0xD9,       // 0 = nearest, 1 = maximum goal
     CMD_SIGNAL_INTERVAL = 0xDA,     // range 5-20, unit 40 ms
     CMD_RESET = 0xDE,               // reset, 0
-    CMD_DATA_RESPONSE_TIME = 0xE6   // range 0-20, unit 40 ms
-    // etc
+    CMD_DATA_RESPONSE_TIME = 0xE6,  // range 0-20, unit 40 ms
+    CMD_EXTREME_VALUE_STATS = 0xE7, // unit 'times'
+    CMD_EXTREME_FILTER_TIMES = 0xE8,// unit 'times'
+    CMD_NUMBER_OF_SWIPES = 0xE9,    // unit 'times'
+    CMD_PROTOCOL_TYPE = 0xF6,       // 0 = ASCII, 1 = hex protocol 1, 6 = standard protocol
+    CMD_PROPORTION_STATISTIC = 0xF9,// 0-100
+    CMD_INVALID_DISTANCE = 0xFA,    // unit 'cm'
+    CMD_PERCENTAGE = 0xFB,          // unit '%'
+    CMD_FIRMWARE_UPGRADE = 0xDF,    // 1
+    CMD_QUERY_PARAMETERS = 0xFE     // 0, query all parameters
 } ld303_cmd_t;
 
 // parsing state
 typedef enum {
+    STATE_HEADER_55,
+    STATE_HEADER_A5,
+    STATE_LEN,
+    STATE_DATA,
+    STATE_CHECK
 } state_t;
 
 class Protocol {
 
 private:
     state_t _state;
-
-private:
+    uint8_t _sum;
+    uint8_t _buf[32];
+    uint8_t _len;
+    uint8_t _idx;
 
 public:
-    size_t build_tx(uint8_t *buf, uint8_t cmd, uint16_t data);
+    Protocol();
+
+    // builds a parameter setting command
+    size_t build_command(uint8_t *buf, uint8_t cmd, uint16_t data);
+
+    // queries the module for measurement data (param usually 0xD3)
+    size_t build_query(uint8_t *buf, uint8_t param);
+
+    // processes received data, returns true if measurement data was found
     bool process_rx(uint8_t c, uint8_t cmd);
 
 };
