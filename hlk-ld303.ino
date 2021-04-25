774b7c737f79da343cdaed58955b2053840ad504
Bertrik Sikken <bertrik@sikken.nl>
2021-04-25 15:53:29 +0200

    Parse 0xD3 (measurement result).

--- hlk-ld303.ino
+++ hlk-ld303.ino
@@ -164,6 +164,17 @@ void loop(void)
         if (done) {
             int len = protocol.get_data(buf);
             printhex("<", buf, len);
+            uint8_t cmd = buf[0];
+            switch (cmd) {
+            case 0xD3:
+                int dist = (buf[1] << 8) + buf[2];
+                int pres = buf[4];
+                int k = (buf[5] << 8) + buf[6];
+                int micro = buf[7];
+                int off = buf[8];
+                printf("distance=%3d,present=%d,strength=%5d,micro=%d,off=%d\n", dist, pres, k, micro, off);
+                break;
+            }
         }
     }
 }
